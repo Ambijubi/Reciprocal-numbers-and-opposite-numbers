68adb6824cb6b1637213054ea485a2507c0a0bfd
Stas Dokuchaev <95302846+Ambijubi@users.noreply.github.com>
2022-12-23 12:45:44 +0700

    Add files via upload

---
+++ Reciprocal-numbers-and-opposite-numbers_en.cpp
@@ -0,0 +1,58 @@
+//Programm for checking for reciprocal numbers and opposite numbers
+#include <iostream>
+
+//I use "using" declarations so as not to write the std prefix in commands, cout, cin, endl, and string each time
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+    int awnser;
+    float num1, num2;
+
+    cout << "This is a program to test numbers to see if numbers are mutually negative or opposite." <<endl;
+    //We ask the user to check if the numbers are mutually negative or if the numbers are opposite
+    cout << "Check if the numbers are mutually negative or if the numbers are opposite?" <<endl;
+    cout << "1) Mutually negative\n2) Opposite\nAnswer: ";
+    cin >> awnser;
+    
+    //We ask the user to enter numbers (if you enter a fraction, then only decimal)
+    cout << "Enter the first number:";
+    cin >> num1;
+    cout << "Enter second number:";
+    cin >> num2;
+
+    //Calculationsssssssssssssss
+   switch (awnser)
+     {
+         case 1:
+             if (num1 * num2 == 1)
+             {
+                 cout << "Numbers are mutually negative." <<endl;
+             }
+             else
+             {
+                 cout << "Numbers are not mutually negative." <<endl;
+             }
+             break;
+    
+         case 2:
+    
+             if (num1 + num2 == 0)
+             {
+                 cout << "Numbers are opposite." <<endl;
+             }
+             else
+             {
+                 cout << "Numbers are not opposite." <<endl;
+             }
+             break;
+    
+         default:
+             cout << "Oops... Error: Looks like you entered something wrong, try restarting the program." <<endl;
+             break;
+     }
+     return 0;
+}
---
+++ SMPPL-OpenSource.md
@@ -0,0 +1,12 @@
+# Send Me a Postcard Public License for Open Source
+
+Version 0.2, Dec 2022
+
+## TERMS AND CONDITIONS FOR USE, DISTRIBUTION AND MODIFICATION
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the original author and project.
+2. You must send the author a postcard if you changed the his code and posted it online.
+
+Email: ambijubi_pi314@vk.com
